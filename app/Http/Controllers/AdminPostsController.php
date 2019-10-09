35631ef36d02f71374c666cb67b7cb765301249a
Gilbert Daniel <daniel.gilbert247@gmail.com>
2019-10-09 22:22:53 +0630

    Remove Posts when deleting user

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -90,7 +90,9 @@ class AdminPostsController extends Controller
     public function edit($id)
     {
         //
-        return view('admin.posts.edit');
+        $categories = Category::lists('name','id')->all();
+        $post = Post::findOrFail($id);
+        return view('admin.posts.edit',compact('post','categories'));
     }
 
     /**
@@ -103,6 +105,26 @@ class AdminPostsController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $input = $request->all();
+        if($file = $request->file('photo_id')){
+
+
+            $name = time() . $file->getClientOriginalName();
+
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+
+            $input['photo_id'] = $photo->id;
+
+
+        }
+        Auth::user()->posts()->whereId($id)->first()->update($input);
+
+            return redirect('admin/posts/');
+
     }
 
     /**
@@ -114,5 +136,12 @@ class AdminPostsController extends Controller
     public function destroy($id)
     {
         //
+        $post = Post::findOrFail($id);
+
+        unlink(public_path() . $post->photo->file);
+
+        $post->delete();
+
+        return redirect('/admin/posts');
     }
 }
